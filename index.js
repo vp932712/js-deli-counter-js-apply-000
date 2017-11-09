02cb6de02abb781804480bb8358a22cc4eeba01c
vp932712 Patel <vp932712@gmail.com>
2017-11-09 21:35:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ function currentLine(arr) {
     if (arr.length === 0) {
         return 'The line is currently empty.';
     }
-    
+
     return 'The line is currently: ' + arr.map((person, index) => '' + (index + 1) + '. ' + person)
                                          .join(', ');
 }
-    
+
 currentLine(katzDeliLine)
 
 function nowServing(arr) {
     if (arr.length === 0) {
         return 'There is nobody waiting to be served!';
     }
-    
+
     return ("Currently serving " + arr.shift());
 }
